50f921594ed64f49a7a3f5a3fd06a6d813fb5fdc
Dare-Devill <70199827+Dare-Devill@users.noreply.github.com>
2020-12-10 20:39:00 +0530

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'<Bot Token Here>',
-    bitlyToken:'<bit.ly Token Here>'
+    botToken:'<1453309289:AAFKzBzuRCwFXibbDF9Xl8hDeGXuhCJOdwM>',
+    bitlyToken:'<07d7f7a8280459c4f459659bcb951e7c8c2eade3>'
 };
